39dddd570e225361c010032b9faee086e63d21d9
Corey Crawford <corey.n.crawford@gmail.com>
2018-04-24 11:42:45 -0400

    Changed values
    Changed from val('') to empty()

--- js/main.js
+++ js/main.js
@@ -12,8 +12,8 @@ $('#calc').click(function(){
 
 // Button that clears the subtotal, tax, and total
 $('#clear').click(function(){
-  $('#taxrates').val('');
-  $('#subtotal').val('');
+  $('#taxrates').empty();
+  $('#subtotal').empty();
   $('.taxPlus').hide();
 });
 
@@ -22,43 +22,44 @@ $('#clear').click(function(){
 // Hide the income tax text
 $('.incomeTax').hide();
 
-// Difference in floor/ceiling of each tax bracket
-function diff(max, min) {
-  var difference = max - min;
-  return difference;
-}
-
-// Sum for two parameters
-function dbl(a, b) {
-  var sum = a + b;
-  return sum;
-}
-
-// Sum for three parameters
-function trio(x, y, z) {
-  var sums = x + y + z;
-  return sums;
-}
-
 // Function that calculates the tax that you owe before deductions
 $('#calcFedTax').click(function() {
 
+  // Difference in floor/ceiling of each tax bracket
+  function diff(max, min) {
+    var difference = max - min;
+    return difference;
+  }
+
+  // Sum for two parameters
+  function dbl(a, b) {
+    var sum = a + b;
+    return sum;
+  }
+
+  // Sum for three parameters
+  function trio(x, y, z) {
+    var sums = x + y + z;
+    return sums;
+  }
+
   //Declare variables
   var inc, oneZero, productOne, prodTwo, prodThree, prodFour, prodFive, lessThan1, lessThan2, lessThan3, lessThan4, lessThan5, lessThan6, lessThan7;
-  inc = $('#income').val();
-  oneZero = Pts.tbp1 * Rates.tr1;
-  prodOne = diff(Pts.tbp2, Pts.tbp1) * Rates.tr2;
-  prodTwo = diff(Pts.tbp3, Pts.tbp2) * Rates.tr3;
-  prodThree = diff(Pts.bp4, Pts.tbp3) * Rates.tr4;
-  prodFour = diff(Pts.tbp5, Pts.tbp4) * Rates.tr5;
-  prodFive = diff(Pts.tbp6, Pts.tbp5) * Rates.tr6;
-  lessThan1 = inc * Rates.tr1;
-  lessThan2 = oneZero + ((inc - Pts.tbp1) * Rates.tr2);
-  lessThan3 = oneZero + prodOne + ((inc - Pts.tbp2) * Rates.tr3);
-  lessThan4 = oneZero + dbl(prodOne, prodTwo) + ((inc - Pts.tbp3) * Rates.tr4);
-  lessThan5 = oneZero + trio(prodOne, prodTwo, prodThree) + ((inc - Pts.tbp4) * Rates.tr5);
-  lessThan6 = oneZero + dbl(prodOne, prodTwo) + dbl(prodThree, prodFour) + ((inc - Pts.tbp5) * Rates.tr6);
-  lessThan7 = oneZero + dbl(prodOne, prodTwo) + trio(prodThree, prodFour, prodFive) + ((inc - Pts.tbp6) * Rates.tr7);
+    inc = $('#income').val();
+    oneZero = Pts.tbp1 * Rates.tr1;
+    prodOne = diff(Pts.tbp2, Pts.tbp1) * Rates.tr2;
+    prodTwo = diff(Pts.tbp3, Pts.tbp2) * Rates.tr3;
+    prodThree = diff(Pts.bp4, Pts.tbp3) * Rates.tr4;
+    prodFour = diff(Pts.tbp5, Pts.tbp4) * Rates.tr5;
+    prodFive = diff(Pts.tbp6, Pts.tbp5) * Rates.tr6;
+    lessThan1 = inc * Rates.tr1;
+    lessThan2 = oneZero + ((inc - Pts.tbp1) * Rates.tr2);
+    lessThan3 = oneZero + prodOne + ((inc - Pts.tbp2) * Rates.tr3);
+    lessThan4 = oneZero + dbl(prodOne, prodTwo) + ((inc - Pts.tbp3) * Rates.tr4);
+    lessThan5 = oneZero + trio(prodOne, prodTwo, prodThree) + ((inc - Pts.tbp4) * Rates.tr5);
+    lessThan6 = oneZero + dbl(prodOne, prodTwo) + dbl(prodThree, prodFour) + ((inc - Pts.tbp5) * Rates.tr6);
+    lessThan7 = oneZero + dbl(prodOne, prodTwo) + trio(prodThree, prodFour, prodFive) + ((inc - Pts.tbp6) * Rates.tr7);
+
 
   if ( inc <= Pts.tbp1 ) {
       $('.incomeTax').html("You owe $" + (lessThan1).toFixed(2) + " in federal taxes before deductions." );
@@ -83,5 +84,6 @@ $('#calcFedTax').click(function() {
 // Reset button
 $('#clearIncome').click(function(){
   $('#income').val('');
+  $('.incomeTax').empty();
   $('.incomeTax').hide();
 });
