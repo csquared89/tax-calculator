6fb2b8a90011b28629424cd1940c2636e468b45a
Corey Crawford <corey.n.crawford@gmail.com>
2018-04-26 11:55:14 -0400

    Menu
    Added menu

--- js/main.js
+++ js/main.js
@@ -110,4 +110,21 @@ $(document).ready(() => {
     $(event.currentTarget).siblings().removeClass('active');
     $(event.currentTarget).closest('.notes').children().removeClass('disabled')
   });
+
+  ///////////////////////////////////////////
+  $('.login-button').on('click', () => {
+    $('.login-form').toggle();
+    $('.login-button').toggleClass('button-active');
+  });
+
+  $('.product-photo').on('mouseenter', () => {
+    $(this).addClass('photo-active');
+  }).on('mouseleave', function() {
+    $(this).removeClass('photo-active');
+  });
+
+  $('.menu-button').on('click', () => {
+    $('.menu-button').toggleClass('button-active');
+    $('.nav-menu').toggleClass('hide');
+  });
 });
